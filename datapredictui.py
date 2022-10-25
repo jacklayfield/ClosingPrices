da2431b1d35402e47937ce0320ad5f8fc8385889
jacklayfield <layf100@gmail.com>
2022-10-24 23:33:55 -0400

    Developing default page

--- datapredictui.py
+++ datapredictui.py
@@ -1,23 +1,18 @@
 import sys
+import os
 
-from PySide6 import QtWidgets
-# from PySide2 import QtWidgets
-# from PyQt5 import QtWidgets
-# from PyQt6 import QtWidgets
+# IMPORT MODULES
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
 
-from qt_material import apply_stylesheet
+# INSTACE CLASS
+if __name__ == "__main__":
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+    # Load QML File
+    engine.load(os.path.join(os.path.dirname(__file__), "qml/main.qml"))
 
-# create the application and the main window
-app = QtWidgets.QApplication(sys.argv)
-window = QtWidgets.QMainWindow()
-
-# setup stylesheet
-apply_stylesheet(app, theme='dark_teal.xml')
-
-# run
-window.show()
-
-if hasattr(app, 'exec'):
-    app.exec()
-else:
-    app.exec_()
+    # Check Exit App
+    if not engine.rootObjects():
+        sys.exit(-1)
+    sys.exit(app.exec_())
